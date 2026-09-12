f1a38d0eb3bc8e8cf6a392b18bfc74e0f22c8f47
heavenlydesuka7-commits <heavenlydesuka7@gmail.com>
2026-09-12 19:13:16 +0300

    numpy

---
+++ lesson_7/main.py
@@ -0,0 +1,15 @@
+import numpy as np 
+arr = np.array([1,2,3,4,5])
+#print(arr)
+
+print(arr[0])
+print(arr[0:3])
+print(arr[0])
+print(arr[-2])
+
+for x in arr:
+    print(x)
+
+
+for i in range(len(arr)):
+    print(arr[i])
