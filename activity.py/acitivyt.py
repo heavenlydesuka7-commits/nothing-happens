3e6c596847ea059916f0140a8617b66624e86d1f
heavenlydesuka7-commits <heavenlydesuka7@gmail.com>
2026-09-14 18:15:19 +0300

    activity

---
+++ activity.py/acitivyt.py
@@ -0,0 +1,15 @@
+import numpy as np
+my_list = [10, 20, 30, 40, 50]
+arr = np.array(my_list)
+
+print("array")
+print("Array:", arr)
+
+
+print("indexing Data ")
+
+print("First item  ", arr[0])
+print("Last item  ", arr[-1])
+
+
+print("Slice of middle items [1:4]:", arr[1:4])
